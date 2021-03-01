32d4768084cb1ef4bf0a16a9c1c9316eec879638
Leeyu <38371056+leeyu0911@users.noreply.github.com>
2021-03-01 12:43:50 +0800

    Initial commit

---
+++ chartTest/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace chartTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.featureCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 97);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1040, 514);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(13, 13);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 1;
+            this.loadButton.Text = "button1";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // featureCheckedListBox
+            // 
+            this.featureCheckedListBox.CheckOnClick = true;
+            this.featureCheckedListBox.FormattingEnabled = true;
+            this.featureCheckedListBox.Location = new System.Drawing.Point(1058, 97);
+            this.featureCheckedListBox.Name = "featureCheckedListBox";
+            this.featureCheckedListBox.Size = new System.Drawing.Size(192, 514);
+            this.featureCheckedListBox.TabIndex = 2;
+            this.featureCheckedListBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.featureCheckedListBox_ItemCheck);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(1058, 68);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "button1";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(375, 15);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(677, 76);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 623);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.featureCheckedListBox);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.CheckedListBox featureCheckedListBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ chartTest/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace chartTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            loadButton_Click(null, null);
+
+            Random r = new Random();
+            for (int i = 0; i < rnd.Length; i++)
+            {
+                rnd[i] = new int[1000];
+                for (int j = 0; j < 1000; j++)
+                {
+                    rnd[i][j] = r.Next(10000);
+                }
+            }
+
+            //test
+            for (int i = 0; i < rnd.Length; i++)
+            {
+                textBox1.Text += rnd[i] + "\r\n";
+                for (int j = 0; j < 1000; j++)
+                {
+                    textBox1.Text += Convert.ToString(rnd[i][j]) + " ";
+                }
+                textBox1.Text += "\r\n";
+            }
+        }
+
+        int[][] rnd = new int[10][];
+        
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                featureCheckedListBox.Items.Add($"A{i}");
+            }
+        }
+
+        public static Chart CreatChart(string chartName, int xPoint, int yPoint, int xSize, int ySize)
+        {
+            Chart newChart = new Chart();
+            ChartArea newChartArea = new ChartArea();
+            Legend newLegend = new Legend();
+            Series newSeries = new Series();
+
+            ((System.ComponentModel.ISupportInitialize)(newChart)).BeginInit();
+
+            newChartArea.Name = "ChartArea1";
+            newChart.ChartAreas.Add(newChartArea);
+
+            newLegend.Name = "Legend1";
+            newChart.Legends.Add(newLegend);
+
+            newChart.Name = chartName;
+            newSeries.ChartArea = "ChartArea1";
+            newSeries.ChartType = SeriesChartType.Line;
+            newSeries.Legend = "Legend1";
+            newSeries.Name = "Series1";
+            newChart.Series.Add(newSeries);
+
+            newChart.Location = new Point(xPoint, yPoint);
+            newChart.Size = new Size(xSize, ySize);
+            newChart.TabIndex = 0;
+
+            return newChart;
+        }
+
+
+        Chart[] chartsList = new Chart[10];
+        int[] atFlowChartIndex = new int[10];  //chart 在 flowLayoutPanel 中的位置
+        private void featureCheckedListBox_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (!featureCheckedListBox.GetItemChecked(e.Index))
+            {
+                string checkStr = featureCheckedListBox.Items[e.Index].ToString();
+
+                chartsList[e.Index] = CreatChart("chart1", 0, 0, flowLayoutPanel1.Width, 100);
+                //add data to chart here
+                chartsList[e.Index].Titles.Add(checkStr);
+                chartsList[e.Index].Titles[0].Text = checkStr;
+
+                flowLayoutPanel1.Controls.Add(chartsList[e.Index]);
+                flowLayoutPanel1.ScrollControlIntoView(chartsList[e.Index]);
+
+                //紀錄未來要移除的位置
+                atFlowChartIndex[e.Index] = flowLayoutPanel1.Controls.Count - 1;
+            }
+            else
+            {
+                int removeIndex = atFlowChartIndex[e.Index];  //要移除的 chart 在 flowLayoutPanel 中的位置
+
+                flowLayoutPanel1.Controls.RemoveAt(removeIndex);
+                //清空
+                chartsList[e.Index] = null;
+                atFlowChartIndex[e.Index] = 0;
+
+                //處理移除後 其他 chart 在 flowLayoutPanel 中順序變化
+                for (int i = 0; i < atFlowChartIndex.Length; i++)
+                {
+                    if (atFlowChartIndex[i] >= removeIndex)  //在被移除 chart 的下面 index 全部往上
+                    {
+                        atFlowChartIndex[i] -= 1;
+                    }
+                }
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //flowLayoutPanel1.Controls.Clear();  //會呼叫 featureCheckedListBox_ItemCheck 然後執行原本的清除條件
+            foreach (var i in featureCheckedListBox.CheckedIndices)
+            {
+                textBox1.Text += Convert.ToString(i) + "\r\n";
+                featureCheckedListBox.SetItemChecked((int)i, false);
+            }
+            
+        }
+    }
+}
